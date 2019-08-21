25b176a779eb48c3153389da65872e22d0b0b3e8
xinghong <xinghong.chen@hand-china.com>
2019-08-21 16:37:45 +0800

    修改

--- processInterface_stage_Demo/TestData/JtlMerge.sh
+++ processInterface_stage_Demo/TestData/JtlMerge.sh
@@ -2,13 +2,13 @@
 # get all filename in specified path
 path=*.jtl
 files=$(ls $path)
-echo '<?xml version="1.0" encoding="UTF-8"?>' >>bb.jtl
-echo '<testResults version="1.2">' >>bb.jtl
+echo '<?xml version="1.0" encoding="UTF-8"?>' >> $1.jtl
+echo '<testResults version="1.2">' >> $1.jtl
 for filename in $files
 do
 countnum=`cat $filename | wc -l`
 countnum=`expr $countnum - 1`
 text="3,${countnum}p"
-sed -n ${text} $filename   >>bb.jtl
+sed -n ${text} $filename   >> $1.jtl
 done
-echo "</testResults>" >>bb.jtl
+echo "</testResults>" >> $1.jtl

7ee032f01537edad5e29406bc43a10c295407eb0
xinghong <xinghong.chen@hand-china.com>
2019-08-22 16:25:04 +0800

    压测脚本最终版

--- loadTest/loadTest.sh
+++ loadTest/loadTest.sh
@@ -2,7 +2,7 @@
 echo "我要开始执行shell脚本了"
 time=$1
 java -version
-/opt/jmeter/apache-jmeter-3.2/bin/jmeter.sh -n -t 0806.jmx -l jtl/HLY-loadTest-$time.jtl -e -o report/HLY-loadTest-$time
-cd report
+/opt/jmeter/apache-jmeter-3.2/bin/jmeter.sh -n -t 0806.jmx -l jtl/HLY-loadTest-$time.jtl -e -o LoadTestReport/HLY-loadTest-$time
+cd LoadTestReport
 zip -r  HLY-PerformanceTestingReport-$time.zip   HLY-loadTest-$time
 echo "压缩文件成功"

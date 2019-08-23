5823e89536c0a58de21280dc670c1ede3a5d0694
peng.zhang06 <peng.zhang06@hand-china.com>
2019-08-23 14:27:29 +0800

    保存shell脚本

--- loadTest/loadTest.sh
+++ loadTest/loadTest.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -ilex
 echo "我要开始执行shell脚本了"
 time=$1
+scriptName=$2
 java -version
-/opt/jmeter/apache-jmeter-3.2/bin/jmeter.sh -n -t 0806.jmx -l jtl/HLY-loadTest-$time.jtl -e -o LoadTestReport/HLY-loadTest-$time
+/opt/jmeter/apache-jmeter-3.2/bin/jmeter.sh -n -t $scriptName -l jtl/HLY-loadTest-$time.jtl -e -o LoadTestReport/HLY-loadTest-$time
 cd LoadTestReport
 zip -r  HLY-PerformanceTestingReport-$time.zip   HLY-loadTest-$time
 echo "压缩文件成功"

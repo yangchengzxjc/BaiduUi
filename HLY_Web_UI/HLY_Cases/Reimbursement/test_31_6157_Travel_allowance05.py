473eaacd20bfd3f90ab2ac0adea3955242bb9d56
peng.zhang06 <peng.zhang06@hand-china.com>
2019-09-16 15:44:48 +0800

    腾讯云登录调试

--- HLY_Web_UI/HLY_Cases/Reimbursement/test_31_6157_Travel_allowance05.py
+++ HLY_Web_UI/HLY_Cases/Reimbursement/test_31_6157_Travel_allowance05.py
@@ -1,14 +1,13 @@
-from datetime import datetime
 from time import sleep
-
+import datetime as date
 import pytest
 
-from HLY_Elements.expense.elReimbursement import cause, data_time, select_day, stroke_time, edit_date, user_name, \
+
+from HLY_Elements.expense.elReimbursement import cause, data_time, select_day, user_name, \
     start_Date
-from HLY_Elements.expense.elTravel_allowance import Travel_allowance, add_Travel_allowance, city_travel_input, \
-    select_place, save_travel_allowance, delete_button, delete_frame, confirm_button1, \
-    success_delete, edit, owen_expense, goBackDate
-from HLY_PageObject.API.apis import get_formType, close_auto_route_Calculation, change_subsidy_rule
+from HLY_Elements.expense.elTravel_allowance import Travel_allowance, save_travel_allowance,\
+    owen_expense, goBackDate
+from HLY_PageObject.API.apis import  close_auto_route_Calculation, change_subsidy_rule
 from HLY_PageObject.UI.Process import Process
 from HLY_PageObject.UI.Reimbursement import Reimbursement
 from HLY_PageObject.UI.my_expense.my_expense import My_Expense
@@ -23,9 +22,9 @@ def config_env(request, enter):
     :return:
     """
     # 报销单自动获取补贴方式 : 报销单头起止日期生成补贴
-    close_auto_route_Calculation(allowanceAttachExpenseReportDisable=True)
+    close_auto_route_Calculation(allowanceAttachExpenseReportDisable="true")
     # 删除差补中的城市
-    change_subsidy_rule("差旅申请单-UI自动化", ALLOWANCE_CITY=True)
+    change_subsidy_rule("差旅申请单-UI自动化", ALLOWANCE_CITY=False)
     def clear_env():
         """
         清理环境：修改补贴方式为申请单带入  并且打开拆不规则中的城市
@@ -55,7 +54,7 @@ def test_6404_Travel_allowance(config_env):
     # 选择时间
     driver.click(data_time)
     logger.info("点击时间控件选择时间")
-    day = datetime.datetime.now().day
+    day = date.datetime.now().day
     logger.info("day:%s" % day)
     sleep(2)
     reimbursement.get_elements_click(day-1, select_day)
@@ -63,33 +62,22 @@ def test_6404_Travel_allowance(config_env):
     reimbursement.get_elements_click(day, select_day)
     driver.click(reimbursement.get_parent_xpath("新 建"), timeout=1)
     # 获取报销单的出发日期（开始日期）
-    startDate = driver.get_text(start_Date,timeout=2)
+    userName = driver.get_text(user_name)
+    startDate = driver.get_text(start_Date, timeout=2)
     # 进入差补计算
     reimbursement.get_elements_click(0, Travel_allowance)
     # 判断是否有大行程
     assert driver.is_exist(reimbursement.get_xpath("出差往返日期"))
     # 判断费用归熟人= 单据申请人
-    assert driver.get_text(owen_expense) == driver.get_text(user_name)
+    assert driver.get_text(owen_expense) == userName
     # 获取大行程的时间
     time = driver.getAttribute(goBackDate, "value")
+    logger.info("日期：%s" % time.split(",")[0])
     GMT_FORMAT = '%a %b %d %Y %H:%M:%S GMT+0800'
-    assert startDate in datetime.strptime(time[0], GMT_FORMAT)
-    assert "%s" % (day+1) in driver.get_text(stroke_time)
-    # 检查事由的值带入到行程里的备注
-    assert driver.is_exist(reimbursement.get_xpath("报销单头信息自动生成补贴"))
+    from datetime import datetime
+    logger.info("格式化后的时间类型：%s" % (type(datetime.strptime(time.split(",")[0], GMT_FORMAT))))
+    assert startDate in str(datetime.strptime(time.split(",")[0], GMT_FORMAT))
     driver.click(save_travel_allowance)
     logger.info("保存差补")
-    driver.click(reimbursement.get_xpath("编辑"),  timeout=5)
-    driver.click(edit_date, timeout=1)
-    logger.info("编辑报销单头，点击日期")
-    reimbursement.get_elements_click(day - 1, select_day)
-    reimbursement.get_elements_click(day + 1, select_day)
-    reimbursement.get_elements_click(0, reimbursement.get_origin_parent_xpath("保 存"))
-    assert driver.is_exist(reimbursement.get_xpath("修改后，补贴费用将重新生成，是否继续"))
-    reimbursement.get_elements_click(1, reimbursement.get_origin_parent_xpath("确 定"))
-    sleep(7)
-    driver.click(reimbursement.get_parent_xpath("删 除"), timeout=4)
-    reimbursement.get_elements_click(1, reimbursement.get_origin_parent_xpath("确 定"))
-
-
-
+    reimbursement.get_elements_click(0, Travel_allowance)
+    # 判断行程信息

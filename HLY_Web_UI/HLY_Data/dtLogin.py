98420f77a4913c9184f3200beee8d9614ce69445
peng.zhang06 <peng.zhang06@hand-china.com>
2020-06-23 10:47:43 +0800

    删除

--- HLY_Web_UI/HLY_Data/dtLogin.py
+++
@@ -1,6 +0,0 @@
-username = '17960000000'
-password = 'hly123456'
-grant_type='password'
-scope='write'
-new_username = "sh000148"
-token_type ="deferred"
--- HLY_Web_UI/HLY_PageObject/UI/display_element.py
+++
@@ -1,8 +0,0 @@
-
-
-def highLightElement(driver,element):
-    #封装好的高亮显示页面元素的方法
-    #使用JavaScript代码将传入的页面元素对象的背景颜色和边框颜色分别
-    #设置为绿色和红色
-    driver.execute_script("arguments[0].setAttribute('style',arguments[1]);",
-                         element,"background:green ;border:2px solid red;")

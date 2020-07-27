0c16c5ef48c01185202485e43c08d2fd107f8fd5
peng.zhang <peng.zhang06@hand-china.com>
2020-07-27 16:23:57 +0800

    做一个测试需要提交Check.java

---
+++ Helios/src/test/java/com/test/api/testcase/Check.java
@@ -0,0 +1,16 @@
+package com.test.api.testcase;
+
+import org.testng.annotations.Test;
+
+/**
+ * @Author peng.zhang
+ * @Date 2020/7/21
+ * @Version 1.0
+ **/
+public class Check {
+
+    @Test
+    public void test(){
+        System.out.println("测试");
+    }
+}

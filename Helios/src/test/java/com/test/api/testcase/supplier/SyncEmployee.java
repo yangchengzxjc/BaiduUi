b0e52b65855c58d9bb4b477bbf4d5fb2a7b6e060
user <dazzle123>
2020-07-29 21:26:53 +0800

    新增添加证件接口，新增证件方法

--- Helios/src/test/java/com/test/api/testcase/supplier/SyncEmployee.java
+++ Helios/src/test/java/com/test/api/testcase/supplier/SyncEmployee.java
@@ -4,9 +4,11 @@ import com.google.gson.JsonObject;
 import com.hand.baseMethod.HttpStatusException;
 import com.hand.basicObject.Employee;
 import com.hand.basicObject.supplierObject.EmployeeDTO;
+import com.hand.basicconstant.CardType;
 import com.test.BaseTest;
+import com.test.api.method.EmployeeLogin;
 import com.test.api.method.Infra.EmployeeManagePage;
-import jdk.internal.org.jline.utils.Status;
+import com.test.api.method.InfraStructure;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
@@ -29,17 +31,20 @@ public class SyncEmployee extends BaseTest {
     public void addEmployee() throws HttpStatusException {
         EmployeeDTO employeeDTO = new EmployeeDTO();
         JsonObject empObject = employeeManagePage.addEmployee(employee,"甄滙消费商测试公司1","测试部门A","0002","测试接口新建","职务01","级别A");
-        if (empObject.get("status").toString() == "1001") {
-            employeeDTO.setStatus("1");
-            }
-        else{
-            employeeDTO.setStatus("0");
-        }
-        employeeDTO.setFullName(empObject.get("fullName").toString());
-        employeeDTO.setEmployeeID(empObject.get("employeeID").toString());
-        employeeDTO.setMobile(empObject.get("mobile").toString());
-        employeeDTO.setEmail(empObject.get("email").toString());
-//        employeeDTO.setName(empObject.get("name").toString());  身份证名字
+        String userOID=empObject.get("userOID").getAsString();
+        JsonObject userCardInfo=employeeManagePage.addUserCard(employee,userOID,CardType.CHINA_ID,"身份证名字",true);
+
+//        if (empObject.get("status").toString() == "1001") {
+//            employeeDTO.setStatus("1");
+//            }
+//        else{
+//            employeeDTO.setStatus("0");
+//        }
+//        employeeDTO.setFullName(empObject.get("fullName").toString());
+//        employeeDTO.setEmployeeID(empObject.get("employeeID").toString());
+//        employeeDTO.setMobile(empObject.get("mobile").toString());
+//        employeeDTO.setEmail(empObject.get("email").toString());
+//        employeeDTO.setName(userCardInfo.get("lastName").toString()); // 身份证名字
 
     }
 

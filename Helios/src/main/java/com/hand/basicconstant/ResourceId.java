835174efb3b61ac57480bb0d95090b5cf8ac267b
peng.zhang06 <peng.zhang06@hand-china.com>
2020-07-29 15:00:31 +0800

    添加resource Id

---
+++ Helios/src/main/java/com/hand/basicconstant/ResourceId.java
@@ -0,0 +1,15 @@
+package com.hand.basicconstant;
+
+/**
+ * @Author ： xinghong.chen
+ * @Email: xinghong.chen@huilianyi.com
+ * @DATE : 2019/1/24 16:20
+ **/
+
+public class ResourceId {
+
+    public static final String INFRA = "660013000300";
+    public static final String EXPENSE_REPORT = "66000200";
+    public static final String EXPENSE ="66000100";
+    public static final String APPLICATION = "66000300";
+}

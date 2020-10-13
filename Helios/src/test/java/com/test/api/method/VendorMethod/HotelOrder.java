25687960a17b581d6e64f0549c6c8e420909f140
peng.zhang <peng.zhang06@hand-china.com>
2020-10-13 11:32:04 +0800

    1.新增酒店订单信息方法

--- Helios/src/test/java/com/test/api/method/VendorMethod/HotelOrder.java
+++ Helios/src/test/java/com/test/api/method/VendorMethod/HotelOrder.java
@@ -1,8 +1,15 @@
 package com.test.api.method.VendorMethod;
 
+import com.google.gson.JsonObject;
+import com.hand.basicObject.Employee;
+import com.hand.basicObject.supplierObject.hotelOrderInfo.HotelBaseOrder;
 import com.hand.basicObject.supplierObject.hotelOrderInfo.HotelExceedInfo;
 import com.hand.basicObject.supplierObject.hotelOrderInfo.HotelPassengerInfo;
+import com.hand.utils.RandomNumber;
+import com.hand.utils.UTCTime;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -54,5 +61,75 @@ public class HotelOrder {
                 .build();
         return hotelPassengerInfo;
     }
+
+    /**
+     * 酒店订单
+     * @param employee
+     * @param orderType
+     * @param orderNo
+     * @param supplierName
+     * @param supplierCode
+     * @param tmcdata
+     * @param applicat
+     * @param city
+     * @param passengerName
+     * @param cityCode 城市code
+     * @return
+     */
+    public HotelBaseOrder setHotelBaseOrder(Employee employee,String orderType, String orderNo, String supplierName, String supplierCode, JsonObject tmcdata,JsonObject applicat,String city,String passengerName,String cityCode){
+        ArrayList<String> bookerDepartments = new ArrayList<>();
+        bookerDepartments.add(applicat.get("departmentName").getAsString());
+        HotelBaseOrder hotelBaseOrder = HotelBaseOrder.builder()
+                .orderType(orderType)
+                .orderNo(orderNo)
+                .supplierName(supplierName)
+                .supplierCode(supplierCode)
+                .approvalCode(tmcdata.get("approvalCode").getAsString())
+                .orderStatusName("已提交")
+                .orderStatusCode("Submitted")
+                .tenantCode(tmcdata.get("tenantId").getAsString())
+                .tenantName(employee.getTenantName())
+                .employeeId(tmcdata.getAsJsonObject("bookClerk").get("employeeID").getAsString())
+                .bookerDepartments(bookerDepartments)
+                .employeeName(tmcdata.getAsJsonObject("bookClerk").get("name").getAsString())
+                .companyName(employee.getCompanyName())
+                .companyCode(employee.getCompanyCode())
+                .departmentName(applicat.get("departmentName").getAsString())
+                .bookChannel("Online-APP")
+                .bookType("C")
+                .payType("COPAY")
+                .createTime(UTCTime.getBeijingTime(0,0,0))
+                .payTime(UTCTime.getBeijingTime(0,0,2))
+                .successTime(UTCTime.getBeijingTime(0,0,2))
+                .hotelClass("N")
+                .paymentType("M")
+                .accountType("C")
+                .balanceType("P")
+                .guaranteeType("N")
+                .currency("CNY")
+                .totalAmount(new BigDecimal(1000).setScale(2))
+                .contactName(tmcdata.getAsJsonObject("bookClerk").get("name").getAsString())
+                .contactPhone(tmcdata.getAsJsonObject("bookClerk").get("mobile").getAsString())
+                .contactEmail(employee.getEmail())
+                .hotelType("AGR")
+                .hotelName("爱丽丝酒店")
+                .hotelPhone("010-123456")
+                .hotelAddress("上海梅川路25弄")
+                .hotelStar(3)
+                .startTime(UTCTime.getBeijingTime(3,0,0))
+                .endTime(UTCTime.getBeijingDate(5)+" 12:00:00")
+                .lastCancelTime(UTCTime.getBeijingTime(2,0,0))
+                .cityName(city)
+                .cityHeliosCode(cityCode)
+                //为字符串  多个乘客使用,隔开
+                .passengerName(passengerName)
+                .roomName("商务套房")
+                .roomQuantity(1)
+                .roomDays(5)
+                .variance(new BigDecimal(0).setScale(2))
+                .build();
+        return hotelBaseOrder;
+    }
+
 
 }

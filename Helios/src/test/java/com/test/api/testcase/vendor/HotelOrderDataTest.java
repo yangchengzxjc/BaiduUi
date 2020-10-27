1d140a33c596cb3b316e857d6465f1226a674345
peng.zhang <peng.zhang06@hand-china.com>
2020-10-27 11:23:50 +0800

    bug 回归完成修改了部分字段

--- Helios/src/test/java/com/test/api/testcase/vendor/HotelOrderDataTest.java
+++ Helios/src/test/java/com/test/api/testcase/vendor/HotelOrderDataTest.java
@@ -360,6 +360,7 @@ public class HotelOrderDataTest extends BaseTest {
         mapping.put("employeeId","preEmployeeId");
         mapping.put("costCenter1","costCenter");
         mapping.put("hotelOrderBase","hotelBaseOrder");
+        mapping.put("originalOrderNum","originalOrderNo");
         //进行数据对比
         assert GsonUtil.compareJsonObject(hotelOrderDataObject,hotelOrder,mapping);
         if(hotelOrder.getAsJsonObject("hotelBaseOrder").get("preEmployeeOId").isJsonNull()){
